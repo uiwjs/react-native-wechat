56ceded9b86cd3c1567f6d74b62e5f130cb5b1fa
jaywcjlove <398188662@qq.com>
2020-08-01 21:29:37 +0800

    fix(Android): Fix registerApp param issue.

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@
  * 向微信注册应用
  * 必须先注册应用，在 Android 后面的调用才会起作用
  * @param appid 通过微信开放平台，[获取appid](https://open.weixin.qq.com/)
- * @param universalLink Universal Link(通用链接)是苹果在 iOS9 推出的，一种能够方便的通过传统 HTTPS 链接来启动 APP 的功能，可以使用相同的网址打开网址和 APP。
+ * @param universalLink 参数在 iOS 中有效，Universal Link(通用链接)是苹果在 iOS9 推出的，一种能够方便的通过传统 HTTPS 链接来启动 APP 的功能，可以使用相同的网址打开网址和 APP。
  */
 export function registerApp(appid: string, universalLink: string): void;
 /**

f1d0bc8c0d40003d0387f876aac5a5bf69ba0348
jaywcjlove <398188662@qq.com>
2020-08-01 21:26:56 +0800

    feat(Android): Add registerApp props.

--- android/src/main/java/com/uiwjs/react/wechat/RNWechatModule.java
+++ android/src/main/java/com/uiwjs/react/wechat/RNWechatModule.java
@@ -4,10 +4,15 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
 
 public class RNWechatModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private String appId;
+    private IWXAPI api = null;
 
     public RNWechatModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -20,8 +25,13 @@ public class RNWechatModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void registerApp(String appid, Promise promise) {
+        try {
+            this.appId = appid;
+            api = WXAPIFactory.createWXAPI(reactContext.getApplicationContext(), appid, true);
+            promise.resolve(api.registerApp(appid));
+        } catch (Exception e) {
+            promise.reject("-1", e.getMessage());
+        }
     }
 }
